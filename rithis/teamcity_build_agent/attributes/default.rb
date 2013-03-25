f5d18dd6ca88e270eec25b2f65d1c7778159cd32
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2013-03-26 03:18:06 +0400

    Keep legacy nodejs on rithis_teamcity_build_agent

--- rithis/teamcity_build_agent/attributes/default.rb
+++ rithis/teamcity_build_agent/attributes/default.rb
@@ -1,1 +1,3 @@
 override["teamcity_server"]["build_agent"]["server"] = "teamcity.rithis.com"
+override["nodejs"]["install_method"] = "package"
+override["nodejs"]["legacy_packages"] = true
--- rithis/teamcity_build_agent/metadata.rb
+++ rithis/teamcity_build_agent/metadata.rb
@@ -1,5 +1,5 @@
 name    "rithis_teamcity_build_agent"
-version "0.0.1"
+version "0.0.2"
 
 %w{ mongodb nodejs teamcity_server }.each do |cb|
   depends cb
